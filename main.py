7d131f4006c7666606f44f7e95f1407cf0cff753
Vinod1999 <109039225+vinodyeldur@users.noreply.github.com>
2023-02-17 14:51:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print(type("Hello Hi!"))
+
+print("Hello" + " " + "Vinod, welcome to 100 days of Coding" )
+new = (str(len(input("what is your name\n") ) ) ) 
+print("Your name has " + new + " character" )
+print(type("your name has " + new + " character"))
