c1a5265e653192d75e5d4fd40906d3cdddfc9c2a
Vinod1999 <109039225+vinodyeldur@users.noreply.github.com>
2023-02-18 02:59:56 +0000

    Odd Or Even new code

---
+++ ODD or EVEn,.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line
+
+number_check = number / 2
+if number_check != 0:
+    print("This is an odd number.")
+else:
+    print("This is even number.")   
