252d144a04031c7e12a9799ec8ae1b4249dfbb03
Vinod1999 <109039225+vinodyeldur@users.noreply.github.com>
2023-02-20 05:03:43 +0000

    Initial commit

--- main.py
+++ main.py
@@ -2,13 +2,10 @@
 
 age = input("What is your current age? ")
 
-remaining = 90-int(age)
-Days = int(remaining)*365 
-months =int(remaining)*12
-weeks = int(remaining)*52
+remaining = 90 - int(age)
+Days = int(remaining) * 365
+months = int(remaining) * 12
+weeks = int(remaining) * 52
 
-message =  (f"You have {Days} days, {weeks} weeks, and {months} months left")
+message = (f"You have {Days} days, {weeks} weeks, and {months} months left")
 print(message)
-
-
-
